fbdf28b075b6e23afdedfb172bbc5a816f0a2196
mingzhi,zhang <373027385@qq.com>
2015-10-20 16:32:37 +0800

    1
    1

--- server-side/node/expApp.js
+++ server-side/node/expApp.js
@@ -9,7 +9,14 @@
 	/*
 		中间件;
 	*/
+	//全部请求都会通过这个中间件,注意USE方法传入的参数只有回调函数，没有监听的路径，这说明是全局中间件
 
+	app.use(function(req,res,next){
+		console.log(" all tranffic must to throut me");
+		next();
+	});
+
+	//监听网站根目录的中间件；
 	app.use('/',function(req,res,next){
 		console.log('this is middleware');
 		next();
@@ -18,6 +25,9 @@
 	/*
 		路由系列
 	*/
+
+
+
 	// ‘/’根目录
 	app.get('/',resp); 
 	// ‘/fuck’访问 127.0.0.1:8888/fuck
