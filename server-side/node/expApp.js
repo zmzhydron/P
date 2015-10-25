f675cc292fce269167d4f5bf54de62423db4aef8
mingzhi,zhang <373027385@qq.com>
2015-10-25 21:35:34 +0800

    1
    1

--- server-side/node/expApp.js
+++ server-side/node/expApp.js
@@ -1,10 +1,7 @@
 +function(){
 	var exp = require('express');
 	var app = exp();
-	var router = exp.Router();
-	function resp(req,res){
-		res.send('hellow asd asd as d');
-	}
+
 
 	/*
 		中间件;
@@ -27,36 +24,6 @@
 	*/
 	//通过Router来通过一个单独的文件来写路由处理；
 	app.use(require('./route'));
-
-	// ‘/’根目录
-	app.get('/',resp); 
-	// ‘/fuck’访问 127.0.0.1:8888/fuck
-	app.get('/fuck',function(req,res){
-		res.send("this is fuck page")
-	})
-	// get '1.txt'; 使用next方法将控制权交给下一个控制器来处理，问题是好像send只能在第一次使用的时候有效，之后的都无效了；
-	app.get('/1.txt',function(req,res,next){
-		// res.send('txt');
-		next();
-	},function(req,res,next){
-		// res.send('im form next you~~');
-		next();
-	},function(req,res){	
-		res.send('fuck you man~~');
-	});
-	//next方法可以采用一个数组的方法的形式来调用；
-	function N1(req,res,next){
-		console.log('this is N1');
-		next();
-	}
-	function N2(req,res,next){
-		next();
-	}
-	function N3(req,res,next){
-		console.log('this is N3')
-		res.send('i am a function array\'s result');
-	}
-	app.get('/fnArray',[N1,N2,N3]);
 
 	var server = app.listen(8888,function(){
 		var address = server.address().address;
--- server-side/node/route.js
+++ server-side/node/route.js
@@ -1,7 +1,33 @@
 +function(){
 	var route = require('express').Router();  //路由子模块；
-	route.get('/jaja',function(req,res){
-		res.send("i'm jaja form star war XII");
+
+	
+	// ‘/fuck’访问 127.0.0.1:8888/fuck
+	route.get('/fuck',function(req,res){
+		res.send("this is fuck page")
+	})
+	// get '1.txt'; 使用next方法将控制权交给下一个控制器来处理，问题是好像send只能在第一次使用的时候有效，之后的都无效了；
+	route.get('/1.txt',function(req,res,next){
+		// res.send('txt');
+		next();
+	},function(req,res,next){
+		// res.send('im form next you~~');
+		next();
+	},function(req,res){	
+		res.send('fuck you man~~');
 	});
+	//next方法可以采用一个数组的方法的形式来调用；
+	function N1(req,res,next){
+		console.log('this is N1');
+		next();
+	}
+	function N2(req,res,next){
+		next();
+	}
+	function N3(req,res,next){
+		console.log('this is N3')
+		res.send('i am a function array\'s result');
+	}
+	route.get('/fnArray',[N1,N2,N3]);
 	module.exports = route;
 }()
