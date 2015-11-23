ef561e92647cb99507f4cc9bf7a8a30279ebba32
mingzhi,zhang <373027385@qq.com>
2015-11-23 16:25:24 +0800

    1
    1

--- localtest/js/lib/tools.js
+++ localtest/js/lib/tools.js
@@ -24,7 +24,21 @@ define(['jquery'],function($){
 				}
 				return receive;
 			}
-		}
+		},
+		extend:function(subClass,superClass){
+			function F(){}
+			F.prototype = superClass.prototype;
+			subClass.prototype = new F();
+			subClass.prototype.constructor = subClass;
+			subClass.super = superClass.prototype;
+
+
+			//对应与多次继承下，superclass的constructor都已经被改变了,已经不指向superClass，而是指向Object.prototype.constructor
+			//这时需要重新指向其构造函数
+			if(superClass.prototype.constructor === Object.prototype.constructor){
+				superClass.prototype.constructor = superClass;
+			}
+		},
 	}
 	return OOP;
 })
