cbc62549335a43ac6ab70313cdc0e2afe5a8cc4f
zmzhyron <zmzhydron@outlook.com>
2016-01-12 22:22:44 +0800

    this_is_webpacktest

---
+++ server-side/resource/javascript/zmz.js
@@ -0,0 +1,8 @@
+var $ = require('jquery');
+$(document).ready(function(){
+    $("body").append("<div id='a'>hi:~~~~~ "+ require('./zmz1.js') +"</div>");
+});
+// var css = require("!style!css!../css/1-12.css");
+var css = require("!style!css!../css/1-12.css");
+console.log(css);
+console.log('heloo world~~');
